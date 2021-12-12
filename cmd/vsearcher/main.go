b6e660ae2d672f7321008a6433bcde64724651c3
t1732 <oo1732oo@gmail.com>
2021-12-12 22:20:06 +0900

    [refactor] graceful shutdown

--- cmd/vsearcher/main.go
+++ cmd/vsearcher/main.go
@@ -1,6 +1,14 @@
 package main
 
 import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"github.com/gin-gonic/gin"
 	"github.com/t1732/vsercher/internal/handler"
 	"github.com/t1732/vsercher/internal/infrastructure/dao"
@@ -18,5 +26,33 @@ func main() {
 	router := gin.Default()
 	router.GET("/ping", handler.NewPing().Show)
 	router.GET("/vtubers", handler.NewVtuber().Index)
-	router.Run()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	log.Println("running gin server " + port + " port")
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: router,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown:", err)
+	}
+
+	log.Println("Server exiting")
 }
