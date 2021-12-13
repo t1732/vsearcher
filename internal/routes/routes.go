aee313e25c9bd0c5e2f7ece76c05d6a080a79c82
t1732 <oo1732oo@gmail.com>
2021-12-13 10:54:32 +0900

    [feat] no route 時のレスポンスを追加

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,6 +1,8 @@
 package routes
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/t1732/vsercher/internal/handler"
 )
@@ -8,6 +10,10 @@ import (
 func Router() *gin.Engine {
 	router := gin.Default()
 	router.Use(gin.Logger())
+
+	router.NoRoute(func(c *gin.Context) {
+		c.JSON(http.StatusNotFound, gin.H{"message": "Page not found"})
+	})
 
 	router.GET("/ping", handler.NewPing().Show)
 
